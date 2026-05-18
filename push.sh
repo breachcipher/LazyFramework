22cb3b1a2e53c8ba9b3bf15a89b13d842d0c590e
breachcipher <lynxsaiko7@gmail.com>
2026-05-18 04:31:50 +0000

    Update

--- push.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-git remote set-url origin https://github.com/breachcipher/LazyFramework.git
-git init
-git add .
-git commit -m "Update"
-git branch -M main
-git remote add origin https://github.com/breachcipher/LazyFramework.git
-git push -u -f origin main
